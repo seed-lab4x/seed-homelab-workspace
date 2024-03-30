c373117fd1c7076990cf9afaa8ecddc519de8a48
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2024-03-30 22:11:17 +0800

    Update ansible.install support ssh-client

--- .seed/bash/ansible.install.sh
+++ .seed/bash/ansible.install.sh
@@ -13,7 +13,7 @@ then
     then
         sudo apt update
         sudo apt install -y python3-pip
-    elif [[ -n "$(command -v apt)" ]];
+    elif [[ -n "$(command -v yum)" ]];
     then
         sudo yum update
         sudo yum install -y python3-pip
@@ -21,10 +21,26 @@ then
         echo "Please install pip"
         return 1
     fi
+fi
 
-    sudo pip config set global.break-system-packages true
-    pip config set global.break-system-packages true
-    pip install --upgrade pip
+sudo pip config set global.break-system-packages true
+pip config set global.break-system-packages true
+pip install --upgrade pip
+
+if [[ -n "$(command -v ssh)" ]];
+then
+    if [[ -n "$(command -v apt)" ]];
+    then
+        sudo apt update
+        sudo apt install -y openssh-client
+    elif [[ -n "$(command -v yum)" ]];
+    then
+        sudo yum update
+        sudo yum install -y openssh-clients
+    else
+        echo "Please install openssh-client"
+        return 1
+    fi
 fi
 
 if [[ -z "$(command -v ansible)" ]];
