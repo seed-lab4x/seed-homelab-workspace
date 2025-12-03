160afa0e9ce76306a42e861b59c9d3d03a7f7287
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2025-12-03 09:45:29 +0800

    Update bash subrepo.install to install git first

--- .seed/bash/subrepo.install.sh
+++ .seed/bash/subrepo.install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [[ -z "$( command -v git )" ]];
+then
+    echo "Install git"
+
+    if [[ -n "$( command -v git )" ]];
+    then
+        sudo apt update
+        sudo apt install git
+    elif [[ -n "$( command -v yum )" ]];
+    then
+        sudo yum update
+        sudo yum install git
+    else
+        echo "You need install git"
+        return 1
+    fi
+fi
+
 if [[ ! -d $HOME/git-subrepo ]];
 then
     git clone https://github.com/ingydotnet/git-subrepo $HOME/git-subrepo
